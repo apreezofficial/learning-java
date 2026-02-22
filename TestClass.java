fb31df10122999c8d3e1d9016b37095246e1657a
apcodesphere <apreezofficial@gmail.com>
2026-02-22 21:23:20 +0100

    Add MyClass and TestClass with basic Java examples and .gitignore for notes.md

---
+++ TestClass.java
@@ -0,0 +1,40 @@
+public class TestClass{
+    public static void main(String[] args){
+       System.out.println("Hello World");
+        System.out.println("Do you know php is the best");
+        System.out.println("stack");
+        /* js is the best stack ever
+        I commented this out, cause na lie 😒😒 */
+        //string var
+        String name = "ap";
+        System.out.println(name);
+        //int var
+        int num = 100;
+        System.out.println(num);
+        //int decalre and assign
+        int num2;
+        num2 = 200;
+        System.out.println(num2);
+        //constant var
+        final String name2 = "app";
+        System.out.println(name2);
+        //add strings
+        String firstname = "Precious";
+        String lastname = "Adedokun";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+        //char
+        char test = 'm';
+        System.out.println(test);
+        //boolean
+        boolean isJavaFun = true;
+        boolean isPhpFun = true;
+        boolean isJsFun = false;
+        System.out.println(isJavaFun);
+        System.out.println(isPhpFun);
+        char a = 65, b = 66, c = 67;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+    }
+}
