e38ba97bd83a16fc1529e4bb5e593a55fc961b4f
apcodesphere <apreezofficial@gmail.com>
2026-02-23 17:04:52 +0100

    Add initial Java classes and project configuration files

---
+++ IfClass.java
@@ -0,0 +1,27 @@
+public class IfClass {
+    public static void main (String[] args){
+        boolean javascript_easy = false;
+        boolean java_easy = true;
+        boolean php_easy = true;
+        if(javascript_easy){
+            System.out.println("Javacript is easy");
+        } else{
+            System.out.println("Jvascript Hard");
+        }
+        //time if else conditions
+        int time = 20;
+        if(time < 10 ){
+            System.out.println("Good Day");
+        }
+        else if(time < 20){
+            System.out.println("Good Afternoon");
+        }
+        else{
+            System.out.println("Good Evening");
+        }
+        //shorthand
+        time = 18;
+       String result = (time > 18) ? "Good Day" : "Good Eevening";
+       System.out.println(result);
+    }
+}
--- TestClass.java
+++ TestClass.java
@@ -32,9 +32,5 @@ public class TestClass{
         boolean isJsFun = false;
         System.out.println(isJavaFun);
         System.out.println(isPhpFun);
-        char a = 65, b = 66, c = 67;
-System.out.println(a);
-System.out.println(b);
-System.out.println(c);
     }
 }
